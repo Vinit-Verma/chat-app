a96b816ebfb5614414f01f5fad34386787741c30
Vinit <vermavinit64@gmail.com>
2022-05-19 15:06:46 +0530

    second commit

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,9 +1,19 @@
 // Node server to handle io connections.
 // const io = require("socket.io")(8000);
+const { instrument } = require("@socket.io/admin-ui");
 const io = require("socket.io")(8000, {
   cors: {
     origin: "*",
   },
+});
+
+instrument(io, {
+  auth: {
+    type: "basic",
+    username: "admin",
+    password: "$2b$10$heqvAkYMez.Va6Et2uXInOnkCT6/uQj1brkrbyG3LpopDklcq7ZOS", // "changeit" encrypted with bcrypt
+  },
+  readonly: true,
 });
 
 const users = {};
